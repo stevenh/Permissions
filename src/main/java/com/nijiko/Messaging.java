24add44a0b293914889e7e45778a738074ffcc1d
FrozenCow <frozencow@gmail.com>
2011-03-16 11:36:39 +0100

    Changed Messaging.java to be fully UTF-8.

--- src/main/java/com/nijiko/Messaging.java
+++ src/main/java/com/nijiko/Messaging.java
@@ -56,11 +56,11 @@ public class Messaging {
     }
 
     /**
-     * Parses the original string against color specific codes. This one converts &[code] to ยง[code]
+     * Parses the original string against color specific codes. This one converts &[code] to \u00A7[code]
      * <br /><br />
      * Example:
      * <blockquote><pre>
-     * Messaging.parse("Hello &2world!"); // returns: Hello ยง2world!
+     * Messaging.parse("Hello &2world!"); // returns: Hello \u00A72world!
      * </pre></blockquote>
      *
      * @param original The original string used for conversions.
@@ -69,7 +69,6 @@ public class Messaging {
      */
     public static String parse(String original) {
 	original = colorize(original);
-	//Optionally change the \u00A7 back to ยง and tack .replace(String.valueOf((char)194), "") onto end of line
 	return original.replaceAll("(&([a-z0-9]))", "\u00A7$2").replace("&&", "&");
     }
 
@@ -79,7 +78,7 @@ public class Messaging {
      * Color codes allowed: black, navy, green, teal, red, purple, gold, silver, gray, blue, lime, aqua, rose, pink, yellow, white.</p>
      * Example:
      * <blockquote<pre>
-     * Messaging.colorize("Hello &lt;green>world!"); // returns: Hello ยง2world!
+     * Messaging.colorize("Hello <green>world!"); // returns: Hello \u00A72world!
      * </pre></blockquote>
      *
      * @param original Original string to be parsed against group of color names.
@@ -87,8 +86,8 @@ public class Messaging {
      * @return <code>String</code> - The parsed string after conversion.
      */
     public static String colorize(String original) {
-    //Removed the weird ย character
-	return original.replace("<black>", "ง0").replace("<navy>", "ง1").replace("<green>", "ง2").replace("<teal>", "ง3").replace("<red>", "ง4").replace("<purple>", "ง5").replace("<gold>", "ง6").replace("<silver>", "ง7").replace("<gray>", "ง8").replace("<blue>", "ง9").replace("<lime>", "งa").replace("<aqua>", "งb").replace("<rose>", "งc").replace("<pink>", "งd").replace("<yellow>", "งe").replace("<white>", "งf");
+    //Removed the weird character
+	return original.replace("<black>", "\u00A70").replace("<navy>", "\u00A71").replace("<green>", "\u00A72").replace("<teal>", "\u00A73").replace("<red>", "\u00A74").replace("<purple>", "\u00A75").replace("<gold>", "\u00A76").replace("<silver>", "\u00A77").replace("<gray>", "\u00A78").replace("<blue>", "\u00A79").replace("<lime>", "\u00A7a").replace("<aqua>", "\u00A7b").replace("<rose>", "\u00A7c").replace("<pink>", "\u00A7d").replace("<yellow>", "\u00A7e").replace("<white>", "\u00A7f");
     }
 
     /**
